f0e642f9d88d2219ffca245e2fd733feff5bbc8b
elllephant <elllephant@hotmail.com>
2019-05-15 16:58:09 -0400

    package published

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,10 @@ module.exports = ({ config }) => {
 
     config.module.rules.push({
         test: /tsx?$/,
-        loader: 'awesome-typescript-loader'
+        loader: 'awesome-typescript-loader',
+        options: {
+            configFileName: 'tsconfig.dev.json'
+        }
     })
 
     config.resolve.extensions.push('.ts', '.tsx')
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import Wheel from "./component/Wheel"
+import { IWheelItem, IWheel } from './wheel';
+import placeOnWheel from './wheel';
+import { IitemRenderConfig, IWheelProps } from './component/Wheel';
+
+export {
+    IWheelItem,
+    IWheel,
+    placeOnWheel,
+    IitemRenderConfig,
+    IWheelProps,
+}
 
 export default Wheel
